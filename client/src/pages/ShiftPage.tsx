0aabe3c67411e70d8d77a21207581b24cb696912
RiverStephenson <riverstephenson06@outlook.com>
2024-11-06 12:55:01 -0600

    marys maddness

---
+++ client/src/pages/ShiftPage.tsx
@@ -0,0 +1,9 @@
+
+export default function ShiftPage() {
+    return (
+        <>
+        <h1>Welcome</h1>
+        <div>shift</div>
+        </>
+    )
+}
---
+++ client/src/pages/WeatherPage.tsx
@@ -0,0 +1,9 @@
+
+export default function WeatherPage() {
+    return (
+        <>
+        <h1>Welcome</h1>
+        <div>weather</div>
+        </>
+    )
+}
