16b4cfdc7fa5c023e506873d73970bf6e7e3689e
Meez <meez@Meezs-MBP.attlocal.net>
2024-11-07 13:39:32 -0600

    added id to the user-seeds.tsfile

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -3,18 +3,21 @@ import { User } from '../models/index.js';
 export const seedUsers = async () => {
   await User.bulkCreate(
     [
-      { username: 'JoshuaWoods', email: 'joshuawoods@guru.com', password: 'password' },
+      { id: 1, username: 'JoshuaWoods', email: 'joshuawoods@guru.com', password: 'password' },
       {
+        id: 1, 
         username: 'SameerIdris',
         email: 'sameeridris@scribe.com',
         password: 'password',
       },
       {
+        id: 1, 
         username: 'RiverStephenson',
         email: 'riverstephenson@comet.com',
         password: 'password',
       },
       {
+        id: 1,
         username:'IsakLarsson',
         email: 'isaklarsson@pluto.com', password: 'password',
       },
