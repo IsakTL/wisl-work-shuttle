5eb3aa23045309958434213df08120d756618226
Isak <isaktlarsson99@gmail.com>
2024-11-08 14:08:56 -0600

    Add current MapImage

--- client/src/pages/WeatherPage.tsx
+++ client/src/pages/WeatherPage.tsx
@@ -4,12 +4,10 @@ import NavBar from "../components/NavBar"
 export default function WeatherPage() {
     return (
         <>
+            <NavBar></NavBar>
             <h1>Welcome</h1>
             <div>weather</div>
             <MapImage />
-        <NavBar></NavBar>
-        <h1>Welcome</h1>
-        <div>weather</div>
         </>
     )
 }
