630bf2c661bfb7cf5875d5822c6ddeac39c5bbcd
RiverStephenson <riverstephenson06@outlook.com>
2024-11-09 15:42:52 -0600

    adds login css styles

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { useState, FormEvent, ChangeEvent } from "react";
-
+import './login.css'
 import Auth from "../utils/auth"; // Import the Auth utility for managing authentication state
 import { login } from "../api/authAPI"; // Import the login function from the API
 import { UserLogin } from "../interfaces/UserLogin"; // Import the interface for UserLogin
@@ -42,6 +42,7 @@ const Login = () => {
   };
 
   return (
+    
     <div className="form-container">
       <form className="form login-form" onSubmit={handleSubmit}>
         <h1>Welcome WISL Workers</h1>
@@ -49,7 +50,7 @@ const Login = () => {
         <div className="form-group">
           <label>Employee ID</label>
           <input
-            className="form-input"
+            className="form-input in"
             type="text"
             name="username"
             value={loginData.username || ""}
@@ -60,7 +61,7 @@ const Login = () => {
         <div className="form-group">
           <label>Password</label>
           <input
-            className="form-input"
+            className="form-input in"
             type="password"
             name="password"
             value={loginData.password || ""}
@@ -68,13 +69,16 @@ const Login = () => {
           />
         </div>
         {/* Submit button for the login form */}
-        <div className="form-group">
+        <div className="form-group btn-div">
           <button className="btn btn-primary" type="submit">
             Login
           </button>
         </div>
       </form>
+      <img  src='https://images.pexels.com/photos/385997/pexels-photo-385997.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' className='pic'></img>
+      <footer className='foot'>Crafted with Care, sincerely, Woods, Idris, Stephenson, Larrson,</footer>
     </div>
+    
   );
 };
 
