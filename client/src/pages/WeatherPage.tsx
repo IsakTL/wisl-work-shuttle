79992e10696d33fcf2c1f649794be9ba8e46ce6a
RiverStephenson <riverstephenson06@outlook.com>
2024-11-11 17:47:01 -0600

    Revert "should save shiftData to localStorage"

--- client/src/pages/WeatherPage.tsx
+++ client/src/pages/WeatherPage.tsx
@@ -1,16 +1,14 @@
 import MapImage from '../components/MapImage.tsx';
 import WeatherReport from "../components/Weather"
 import NavBar from "../components/NavBar"
-import './weatherPage.css'
 
 export default function WeatherPage() {
     return (
         <>
             <NavBar></NavBar>
             <h1>Welcome</h1>
-            <div>
             <WeatherReport/>
-            </div>
+            <div>weather</div>
             <MapImage />
             
         </>
