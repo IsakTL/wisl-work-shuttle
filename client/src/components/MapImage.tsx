f9d5c32c9c1ae2af1b2d7391d845f50373002592
Isak <isaktlarsson99@gmail.com>
2024-11-11 21:54:35 -0600

    Add map image

--- client/src/components/MapImage.tsx
+++ client/src/components/MapImage.tsx
@@ -1,9 +1,10 @@
 // const lat = sequelize.data.lat ? 12.123123: 0; // Use fetch
-// require('dotenv').config();
 
 /* Can't use dotenv in this folder without importing from something in '../../server'.
 How do I access environmental variables established in another directory?Should this itself be in the back end, at least partially? */
 import { useState, useEffect } from 'react';
+// import dotenv from 'dotenv';
+// require('dotenv').config(path: __dirname);
 
 const MapImage = () => {
     const [mapSrc, setMapSrc] = useState('');
@@ -12,23 +13,28 @@ const MapImage = () => {
     // Must instead use private variables from sequelize, pulled from database
     const
     pickupLon = -93.26785,
-    pickupLat = 44.98439;
-    // const dropoffLat = 0,
-    // dropoffLon = 0;
+    pickupLat = 44.98439,
+    // dropoffLon = -93.08844,
+    // dropoffLat = 44.94,
+    token = import.meta.env.VITE_MAPBOX_PUBLIC_TOKEN,
+    tempToken = 'pk.eyJ1IjoiaXNha3RsIiwiYSI6ImNtMzRzb28wZTAzYzEyam9zYzk0c2gzZjQifQ.Ogf2AltV9Yv5sCjAbF_wkg';
+    console.log(token); // Undefined, import broken
 
-
-    /* setMapSrc to be used to change mapSrc url */
-
+	/* DO NOT use no-cors
+    "Access to fetch at 'https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/-93.26785,44.98439,15.25,0/400x400?access_token=undefined' from origin 'http://localhost:3000' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled."
+    */
 
     const getImage = async () => {
-        const mapImage = await fetch (`https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/${pickupLon},${pickupLat},15.25,0/400x400?access_token=${import.meta.env.VITE_MAPBOX_PUBLIC_TOKEN}`, {mode: 'no-cors'});
+        const mapImage = await fetch (`https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/${pickupLon},${pickupLat},15.25,0/400x400?access_token=${tempToken}`);
         // console.log(mapImage);
         // const mapURL = await mapImage.json();
         console.log(mapImage);
+
+        // Change mapSrc url
         setMapSrc(mapImage.url);
     }
 
-    useEffect(()=> {getImage}, []);
+    useEffect(()=> {getImage()}, []);
 
     return (
         <div>
