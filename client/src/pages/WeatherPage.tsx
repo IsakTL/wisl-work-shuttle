79839236d3e601bd36d7dd4a676aa646003022c1
RiverStephenson <riverstephenson06@outlook.com>
2024-11-12 22:02:40 -0600

    adds working final

--- client/src/pages/WeatherPage.tsx
+++ client/src/pages/WeatherPage.tsx
@@ -7,11 +7,10 @@ export default function WeatherPage() {
     return (
         <>
             <NavBar></NavBar>
-            <h1>Welcome</h1>
-            <div>Weather:</div>
             <div><WeatherReport/></div>
             <div>Your pickup point:</div>
             <MapImage />
+            <a href='https://www.google.com/maps/dir/Des+Moines,+Iowa/Chuck+E.+Cheese,+13364+Montfort+Dr,+Dallas,+TX+75240/@37.1427908,-100.5025357,6z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x87ee99a4c1611ee7:0x710028512691e4b2!2m2!1d-93.6249522!2d41.5868417!1m5!1m1!1s0x864c20e6e390a8f9:0x4cb3ad0af3fe9eba!2m2!1d-96.8107053!2d32.9310412?entry=ttu&g_ep=EgoyMDI0MTEwNi4wIKXMDSoASAFQAw%3D%3D'>Directions</a>
         </>
     )
 }
